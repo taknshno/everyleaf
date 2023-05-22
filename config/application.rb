db99d3ce2e0ade918ed8550192e551b300942a7c
taknshno <126603921+taknshno@users.noreply.github.com>
2023-05-22 22:38:15 +0900

    [Add] Change the time zone to Tokyo

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ module Manyo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
 
     config.generators do |g|
       g.test_framework :rspec,
