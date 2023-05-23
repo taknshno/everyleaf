e7898964625d6f79f73b303c31b4c47f2d1ec924
taknshno <126603921+taknshno@users.noreply.github.com>
2023-05-23 13:41:31 +0900

    [Fix] Fixed can not register task data related with enum

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,8 @@
 class Task < ApplicationRecord
   validates :task_name,  presence: true, length: { maximum: 60 }
   validates :task_detail,  presence: true, length: { maximum: 256 }
-  validates :status,  inclusion: { in: [1,2,3] }
-  validates :priority,  inclusion: { in: [1,2,3] }
+  validates :status,  inclusion: { in: ['未着手', '着手','完了'] }
+  validates :priority,  inclusion: { in: ['高', '中', '低'] }
 
   enum status: { 未着手: 1, 着手: 2, 完了: 3 }
   enum priority: { 高: 1, 中: 2, 低: 3 }
