a1bb3f0549fbdaa62d14502910f5fe89f11bc23a
taknshno <126603921+taknshno@users.noreply.github.com>
2023-05-24 12:02:23 +0900

    [Add] Add Model Spec and test data for Tasks sort and search.

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -9,6 +9,20 @@ FactoryBot.define do
   factory :second_task, class: Task do
     task_name { "Factoryで作ったデフォルトのタスク名2" }
     task_detail { "Factoryで作ったデフォルトのタスクの詳細2" }
+    status { "着手" }
+    priority { "高" }
+    end_date { "2023-05-01" }
+  end
+  factory :third_task, class: Task do
+    task_name { "Factoryで作ったデフォルトのタスク名3" }
+    task_detail { "Factoryで作ったデフォルトのタスクの詳細3" }
+    status { "未着手" }
+    priority { "高" }
+    end_date { "2023-05-01" }
+  end
+  factory :fourth_task, class: Task do
+    task_name { "Factoryで作ったデフォルトのタスク名4" }
+    task_detail { "Factoryで作ったデフォルトのタスクの詳細4" }
     status { "未着手" }
     priority { "高" }
     end_date { "2023-05-01" }
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -13,18 +13,6 @@ RSpec.describe 'タスクモデル機能', type: :model do
         expect(task).not_to be_valid
       end
     end
-    context 'statusが範囲外の場合' do
-      it 'バリデーションにひっかかる' do
-        task = Task.new(task_name: 'test_name', task_detail: 'test_detail', status: 100, priority: 1, end_date: 2023-05-22)
-        expect(task).not_to be_valid
-      end
-    end
-    context 'priorityが範囲外の場合' do
-      it 'バリデーションにひっかかる' do
-        task = Task.new(task_name: 'test_name', task_detail: 'test_detail', status: 1, priority: 100, end_date: 2023-05-22)
-        expect(task).not_to be_valid
-      end
-    end
     context 'task_nameが入力されており、statusとpriorityが範囲内' do
       it 'バリデーションが通る' do
         task = Task.new(task_name: 'test_name', task_detail: 'test_detail', status: 2, priority: 3, end_date: 2023-05-22)
@@ -32,4 +20,33 @@ RSpec.describe 'タスクモデル機能', type: :model do
       end
     end
   end
+    describe '検索機能' do
+      let!(:task) { FactoryBot.create(:task, task_name: 'task') }
+      let!(:second_task) { FactoryBot.create(:second_task, task_name: "sample") }
+      let(:third_task) { FactoryBot.create(:third_task, task_name: "task", status: "着手") }
+      let(:fourth_task) { FactoryBot.create(:fourth_task, task_name: "sample", status: "未着手") }
+      context 'scopeメソッドでタイトルのあいまい検索をした場合' do
+        it "検索キーワードを含むタスクが絞り込まれる" do
+          expect(Task.word_search('task')).to include(task)
+          expect(Task.word_search('task')).not_to include(second_task)
+          expect(Task.word_search('task').count).to eq 1
+        end
+      end
+      context 'scopeメソッドでステータス検索をした場合' do
+        it "ステータスに完全一致するタスクが絞り込まれる" do
+          expect(Task.status_search('未着手')).to include(task)
+          expect(Task.status_search('未着手')).not_to include(second_task)
+          expect(Task.status_search('未着手').count).to eq 1
+        end
+      end
+      context 'scopeメソッドでタイトルのあいまい検索とステータス検索をした場合' do
+        it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
+          expect(Task.word_search('task').status_search('未着手')).to include(task)
+          expect(Task.word_search('task').status_search('未着手')).not_to include(second_task)
+          expect(Task.word_search('task').status_search('未着手')).not_to include(third_task)
+          expect(Task.word_search('task').status_search('未着手')).not_to include(fourth_task)
+          expect(Task.word_search('task').status_search('未着手').count).to eq 1
+        end
+      end
+    end
 end
