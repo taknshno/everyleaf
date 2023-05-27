bd6eb64b05394421fcc1a45386ec503647dc63df
taknshno <126603921+taknshno@users.noreply.github.com>
2023-05-28 01:03:40 +0900

    [Add] Add index to id on User model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_24_194827) do
+ActiveRecord::Schema.define(version: 2023_05_27_160001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2023_05_24_194827) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["id"], name: "index_users_on_id"
   end
 
   add_foreign_key "tasks", "users"
